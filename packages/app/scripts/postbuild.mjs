ff6f81761d70c2bbd6461d1e86ef0a63a744c619
XLor <yjl9903@vip.qq.com>
2023-04-09 01:25:02 +0800

    fix: app postbuild

--- packages/app/scripts/postbuild.mjs
+++ packages/app/scripts/postbuild.mjs
@@ -7,7 +7,7 @@ const __dirname = path.join(fileURLToPath(import.meta.url), '../../');
 const outputDist = path.join(__dirname, '../../dist');
 const outputFunc = path.join(__dirname, '../../functions');
 
-await fs.rm(outputDist, { recursive: true });
-await fs.rm(outputFunc, { recursive: true });
+await fs.rm(outputDist, { recursive: true }).catch(() => {});
+await fs.rm(outputFunc, { recursive: true }).catch(() => {});
 await fs.copy(path.join(__dirname, 'dist'), outputDist);
 await fs.copy(path.join(__dirname, 'functions'), outputFunc);
