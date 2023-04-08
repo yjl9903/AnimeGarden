b89ec291955928f3444a0f18d247371d9c15fdee
XLor <yjl9903@vip.qq.com>
2023-04-09 00:49:30 +0800

    chore: split fetch script

--- scripts/fetch.ts
+++ scripts/fetch.ts
@@ -4,8 +4,8 @@ import { HttpsProxyAgent } from 'https-proxy-agent';
 
 import { fetchPage } from '../packages/animegarden/src';
 
-async function main() {
-  for (let page = 1; ; page++) {
+async function main(start: number, dist: string) {
+  for (let page = start; ; page++) {
     console.log(`Fetch page ${page}`);
 
     const r = await fetchPage({
@@ -17,11 +17,11 @@ async function main() {
     });
 
     await fs.promises.writeFile(
-      path.join('./data', `${page}.json`),
+      path.join(dist, `${page}.json`),
       JSON.stringify(r, null, 2),
       'utf-8'
     );
   }
 }
 
-main();
+main(1, 'chunk/0');
