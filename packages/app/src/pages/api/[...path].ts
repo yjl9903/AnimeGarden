248da5254aaef42e0d3de8d527579f0c535b5212
XLor <yjl9903@vip.qq.com>
2023-04-09 03:45:44 +0800

    fix: update proxy

--- packages/app/src/pages/api/[...path].ts
+++ packages/app/src/pages/api/[...path].ts
@@ -1,20 +1,10 @@
 import type { APIRoute } from 'astro';
 
-import useReflare from 'reflare';
-
 export const get: APIRoute = async ({ request }) => {
-  const reflare = await useReflare();
-
-  reflare.push({
-    path: '/*',
-    upstream: {
-      domain: 'animegarden.yjl9903.workers.dev',
-      protocol: 'https',
-      onRequest: (request: Request, url: string): Request => {
-        return new Request(url.replace(/^\/api/, ''), request);
-      }
-    }
-  });
-
-  return reflare.handle(request);
+  const url = new URL(request.url);
+  url.protocol = 'https:';
+  url.host = 'animegarden.yjl9903.workers.dev';
+  url.port = '';
+  url.pathname = url.pathname.replace(/^\/api/, '');
+  return fetch(new Request(url, request));
 };
