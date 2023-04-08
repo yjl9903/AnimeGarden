e9e4a7b474a3292bbf3c445637e41c12d9165db3
XLor <yjl9903@vip.qq.com>
2023-04-09 04:56:02 +0800

    feat: upload scripts

--- scripts/d1.ts
+++ scripts/d1.ts
@@ -2,11 +2,34 @@ import fs from 'fs-extra';
 import path from 'path';
 
 import { breadc } from 'breadc';
+
 import { Resource } from '../packages/animegarden/src';
 
 const cli = breadc('d1', { version: '0.0.0' });
 
-cli.command('user', 'Generate user sql').action(async (message, option) => {
+cli.command('resource', 'Generate resources sql').action(async (option) => {
+  const res = await readResources();
+  const lines = [];
+  for (const r of res) {
+    lines.push(`INSERT OR IGNORE INTO resource (title, href, type, magnet, size, createdAt, publisher, fansub)
+    VALUES ('${escape(r.title)}', '${escape(r.href)}', '${escape(r.type)}', '${escape(
+      r.magnet
+    )}', '${escape(r.size)}', '${escape(r.createdAt)}', ${r.publisher.id}, ${
+      r.fansub ? r.fansub.id : 'null'
+    });
+    `);
+  }
+  const sc: string[] = [];
+  const chunkSize = 500;
+  for (let i = 0, p = 0; i < lines.length; p += 1, i += chunkSize) {
+    const chunk = lines.slice(i, i + chunkSize);
+    sc.push(`pnpm -C packages/worker d1:execute data/resource-${p}.sql`);
+    fs.writeFile(`./packages/worker/data/resource-${p}.sql`, chunk.join('\n'), 'utf-8');
+  }
+  await fs.writeFile('upload.ps1', sc.join('\n'), 'utf-8');
+});
+
+cli.command('user', 'Generate user sql').action(async (option) => {
   const res = await readResources();
   const user = new Map<string, Resource['publisher']>();
   for (const r of res) {
@@ -21,7 +44,7 @@ cli.command('user', 'Generate user sql').action(async (message, option) => {
   fs.writeFile('./packages/worker/data/user.sql', lines.join('\n'), 'utf-8');
 });
 
-cli.command('team', 'Generate team sql').action(async (message, option) => {
+cli.command('team', 'Generate team sql').action(async (option) => {
   const res = await readResources();
   const team = new Map<string, Exclude<Resource['fansub'], undefined>>();
   for (const r of res) {
@@ -41,6 +64,10 @@ cli.command('team', 'Generate team sql').action(async (message, option) => {
 });
 
 cli.run(process.argv.slice(2)).catch((err) => console.error(err));
+
+function escape(text: string) {
+  return text.replace(`'`, `''`);
+}
 
 async function readResources(root = 'chunk') {
   const chunks = fs.readdirSync(root);
