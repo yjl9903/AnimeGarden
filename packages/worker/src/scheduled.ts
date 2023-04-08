fb7f8630bf5013923910dc7934162a6ab36b2911
XLor <yjl9903@vip.qq.com>
2023-04-09 05:56:32 +0800

    feat: add missing team and user

--- packages/worker/src/scheduled.ts
+++ packages/worker/src/scheduled.ts
@@ -5,8 +5,48 @@ import { fetchPage } from 'animegarden';
 
 export async function handleScheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext) {
   const database = makeDatabase(env.database);
+  const teams = new Set<number>();
+  const users = new Set<number>();
+
   for (let page = 1; ; page++) {
     const res = await fetchPage({ page, retry: 5 });
+
+    // Check teams and users
+    for (const r of res) {
+      if (!users.has(+r.publisher.id)) {
+        const u = await database
+          .selectFrom('user')
+          .selectAll()
+          .where('id', '=', +r.publisher.id)
+          .executeTakeFirst();
+        if (u) {
+          users.add(u.id);
+        } else {
+          await database
+            .insertInto('user')
+            .values({ id: +r.publisher.id, name: r.publisher.name })
+            .executeTakeFirst();
+          users.add(+r.publisher.id);
+        }
+      }
+      if (r.fansub && !teams.has(+r.fansub.id)) {
+        const u = await database
+          .selectFrom('team')
+          .selectAll()
+          .where('id', '=', +r.fansub.id)
+          .executeTakeFirst();
+        if (u) {
+          teams.add(u.id);
+        } else {
+          await database
+            .insertInto('team')
+            .values({ id: +r.fansub.id, name: r.fansub.name })
+            .executeTakeFirst();
+          teams.add(+r.fansub.id);
+        }
+      }
+    }
+
     const query = database.insertInto('resource').values(
       res.map((r) => ({
         title: r.title,
