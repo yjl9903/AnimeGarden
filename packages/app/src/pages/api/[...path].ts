838762110f084fdac24ee01695802c4770381e94
XLor <yjl9903@vip.qq.com>
2023-04-09 06:41:03 +0800

    fix: app proxy all methods

--- packages/app/src/pages/api/[...path].ts
+++ packages/app/src/pages/api/[...path].ts
@@ -1,6 +1,6 @@
 import type { APIRoute } from 'astro';
 
-export const get: APIRoute = async ({ request }) => {
+export const all: APIRoute = async ({ request }) => {
   const url = new URL(request.url);
   url.protocol = 'https:';
   url.host = 'animegarden.yjl9903.workers.dev';
