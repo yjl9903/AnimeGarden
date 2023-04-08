4ad22bf73fad6be4cb25ae4a9616d5c40e7b2191
XLor <yjl9903@vip.qq.com>
2023-04-09 00:31:26 +0800

    chore: fetch data script

--- scripts/fetch.ts
+++ scripts/fetch.ts
@@ -1,15 +1,27 @@
+import fs from 'node:fs';
+import path from 'node:path';
 import { HttpsProxyAgent } from 'https-proxy-agent';
 
 import { fetchPage } from '../packages/animegarden/src';
 
 async function main() {
-  const r = await fetchPage({
-    page: 1,
-    fetch: {
-      agent: process.env.HTTPS_PROXY ? new HttpsProxyAgent(process.env.HTTPS_PROXY) : undefined
-    }
-  });
-  console.log(r);
+  for (let page = 1; ; page++) {
+    console.log(`Fetch page ${page}`);
+
+    const r = await fetchPage({
+      page,
+      retry: Number.MAX_SAFE_INTEGER,
+      fetch: {
+        agent: process.env.HTTPS_PROXY ? new HttpsProxyAgent(process.env.HTTPS_PROXY) : undefined
+      }
+    });
+
+    await fs.promises.writeFile(
+      path.join('./data', `${page}.json`),
+      JSON.stringify(r, null, 2),
+      'utf-8'
+    );
+  }
 }
 
 main();
