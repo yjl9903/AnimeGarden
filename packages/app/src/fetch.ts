2c377d8bd8e9be81ccd56782f4db93da8dbff211
XLor <yjl9903@vip.qq.com>
2023-04-09 22:24:06 +0800

    feat: app filter fansub and publisher

--- packages/app/src/fetch.ts
+++ packages/app/src/fetch.ts
@@ -2,8 +2,18 @@ import type { Resource } from 'animegarden';
 
 import { WORKER_BASE } from './constant';
 
-export async function fetchResources(page: number) {
-  const resources = await fetch(new URL(`/resources?page=${page}`, 'https://' + WORKER_BASE))
+export async function fetchResources(
+  page: number,
+  options: { fansub?: number; publisher?: number } = {}
+) {
+  const url = new URL(`/resources?page=${page}`, 'https://' + WORKER_BASE);
+  if (options.fansub) {
+    url.searchParams.set('fansub', '' + options.fansub);
+  }
+  if (options.publisher) {
+    url.searchParams.set('publisher', '' + options.publisher);
+  }
+  const resources = await fetch(url, {})
     .then((r) => r.json())
     .then((r: any) => r.resources as Resource[]);
   return resources;
