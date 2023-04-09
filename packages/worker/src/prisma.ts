5c4cf683f6e1d3bb6f83ec6a7212280c22836917
XLor <yjl9903@vip.qq.com>
2023-04-09 16:04:04 +0800

    feat: cache prisma client

--- packages/worker/src/prisma.ts
+++ packages/worker/src/prisma.ts
@@ -2,6 +2,11 @@ import { PrismaClient } from '@prisma/client/edge';
 
 import type { Env } from './types';
 
-export function makePrisma(env: Env) {
-  return new PrismaClient({ datasources: { db: { url: env.DATABASE_URL } } });
+let client: PrismaClient;
+export function makePrisma(env: Env): PrismaClient {
+  if (client) {
+    return client;
+  } else {
+    return (client = new PrismaClient({ datasources: { db: { url: env.DATABASE_URL } } }));
+  }
 }
