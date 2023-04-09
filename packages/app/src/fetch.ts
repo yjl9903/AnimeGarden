de97a35ef52a903977025d6a6566206500a3ebd2
XLor <yjl9903@vip.qq.com>
2023-04-10 01:14:44 +0800

    feat: list types

--- packages/app/src/fetch.ts
+++ packages/app/src/fetch.ts
@@ -1,10 +1,12 @@
 import type { Resource } from 'animegarden';
 
+import { ProxyAgent } from 'undici';
+
 import { WORKER_BASE } from './constant';
 
 export async function fetchResources(
   page: number,
-  options: { fansub?: number; publisher?: number } = {}
+  options: { fansub?: number; publisher?: number; type?: string } = {}
 ) {
   const url = new URL(`/resources?page=${page}`, 'https://' + WORKER_BASE);
   if (options.fansub) {
@@ -13,7 +15,16 @@ export async function fetchResources(
   if (options.publisher) {
     url.searchParams.set('publisher', '' + options.publisher);
   }
-  const resources = await fetch(url, {})
+  if (options.type) {
+    url.searchParams.set('type', options.type);
+  }
+  // @ts-ignore
+  const resources = await fetch(url, {
+    dispatcher:
+      import.meta.env.DEV && import.meta.env.HTTPS_PROXY
+        ? new ProxyAgent(import.meta.env.HTTPS_PROXY)
+        : undefined
+  })
     .then((r) => r.json())
     .then((r: any) => r.resources as Resource[]);
   return resources;
