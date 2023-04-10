c46134d6bcbce7eba0048f44cbf63336d8d19c72
XLor <yjl9903@vip.qq.com>
2023-04-11 01:39:28 +0800

    chore: init tokenizer

---
+++ packages/anitomy/src/tokenizer.ts
@@ -0,0 +1,8 @@
+import type { AnitomyOptions, ParsedResult } from './types';
+
+export function tokenize(filename: string, result: ParsedResult, options: AnitomyOptions) {
+  const tokens: Token[] = [];
+  return tokens;
+}
+
+export class Token {}
