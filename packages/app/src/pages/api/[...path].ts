de6722ee530efd8c1ae6d4114544aa53425a91d6
XLor <yjl9903@vip.qq.com>
2023-05-31 23:28:10 +0800

    fix(app): pass response header

--- packages/app/src/pages/api/[...path].ts
+++ packages/app/src/pages/api/[...path].ts
@@ -10,10 +10,15 @@ export const all: APIRoute = async ({ request }) => {
   url.pathname = url.pathname.replace(/^\/api/, '');
 
   const response = await fetch(new Request(url, request));
+  const headers: [string, string][] = [];
+  response.headers.forEach((value, key) => {
+    headers.push([key, value]);
+  });
+
   return new Response(response.body, {
     headers: {
       'cache-control': request.method === 'GET' ? `public, max-age=3600` : 'no-store',
-      ...Object.fromEntries(response.headers.entries()),
+      ...Object.fromEntries(headers),
       'access-control-allow-origin': '*',
       'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
       'access-control-allow-headers': 'Content-Type, Cache-Control, Authorization'
