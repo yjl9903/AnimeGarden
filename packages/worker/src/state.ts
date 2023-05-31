d6e0606714200fb8dd86dabf56479084c07935d7
XLor <yjl9903@vip.qq.com>
2023-05-31 22:32:36 +0800

    feat(worker): use builtin ttl

--- packages/worker/src/state.ts
+++ packages/worker/src/state.ts
@@ -19,23 +19,18 @@ export function getDetailStore(env: Env) {
 export class KVStore<V> {
   private readonly prefix: string;
 
+  private readonly ttl: number = 1000 * 60 * 60 * 24 * 30;
+
   constructor(private readonly store: KVNamespace, prefix = '') {
     this.prefix = prefix + ':';
   }
 
   async get(key: string): Promise<V | undefined> {
-    const now = new Date();
     try {
       const text = await this.store.get(this.prefix + key);
       if (!!text) {
         const result = JSON.parse(text);
-        const created = new Date(result.timestamp);
-        if (now.getTime() - created.getTime() <= 1000 * 60 * 60 * 24 * 30) {
-          return result.value;
-        } else {
-          await this.remove(key);
-          return undefined;
-        }
+        return result.value;
       } else {
         return undefined;
       }
@@ -45,19 +40,19 @@ export class KVStore<V> {
     }
   }
 
-  async keys(): Promise<string[]> {
-    return (await this.store.list({ prefix: this.prefix })).keys.map((k) => k.name);
+  async keys(options?: Omit<KVNamespaceListOptions, 'prefix'>): Promise<string[]> {
+    return (await this.store.list({ ...options, prefix: this.prefix })).keys.map((k) => k.name);
   }
 
   async has(key: string): Promise<boolean> {
     return !!(await this.store.get(this.prefix + key));
   }
 
-  async put(key: string, value: V): Promise<void> {
-    await this.store.put(
-      this.prefix + key,
-      JSON.stringify({ timestamp: new Date().toISOString(), value })
-    );
+  async put(key: string, value: V, options?: KVNamespacePutOptions): Promise<void> {
+    await this.store.put(this.prefix + key, JSON.stringify({ value }), {
+      expirationTtl: this.ttl,
+      ...options
+    });
   }
 
   async remove(key: string): Promise<void> {
