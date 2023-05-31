95e250046bb41ef10f79683383a652b3f839e207
XLor <yjl9903@vip.qq.com>
2023-05-31 23:32:30 +0800

    chore: fix headers

--- packages/app/src/pages/api/[...path].ts
+++ packages/app/src/pages/api/[...path].ts
@@ -10,15 +10,12 @@ export const all: APIRoute = async ({ request }) => {
   url.pathname = url.pathname.replace(/^\/api/, '');
 
   const response = await fetch(new Request(url, request));
-  const headers: [string, string][] = [];
-  response.headers.forEach((value, key) => {
-    headers.push([key, value]);
-  });
 
   return new Response(response.body, {
     headers: {
       'cache-control': request.method === 'GET' ? `public, max-age=3600` : 'no-store',
-      ...Object.fromEntries(headers),
+      // @ts-ignore
+      ...Object.fromEntries(response.headers.entries()),
       'access-control-allow-origin': '*',
       'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
       'access-control-allow-headers': 'Content-Type, Cache-Control, Authorization'
