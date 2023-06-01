77d9c862c6d9c49b7eeccbc60119ea79ac958a1a
XLor <yjl9903@vip.qq.com>
2023-06-01 19:29:13 +0800

    chore: vite resolve tsconfig poaths

--- packages/app/astro.config.ts
+++ packages/app/astro.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
+
 import uno from 'unocss/astro';
 import react from '@astrojs/react';
 import cloudflare from '@astrojs/cloudflare';
 
 import Info from 'vite-plugin-info';
+import TsconfigPaths from 'vite-plugin-tsconfig-paths';
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,6 +15,6 @@ export default defineConfig({
     mode: 'directory'
   }),
   vite: {
-    plugins: [Info()]
+    plugins: [Info(), TsconfigPaths()]
   }
 });
