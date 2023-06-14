53a301e2dee1b67576f8f708e5ce0b7ea420b704
XLor <yjl9903@vip.qq.com>
2023-06-14 20:42:01 +0800

    feat: handle fetch error

--- packages/animegarden/src/garden/index.ts
+++ packages/animegarden/src/garden/index.ts
@@ -23,7 +23,12 @@ export const DefaultBaseURL = 'https://garden.onekuma.cn/api/';
 export async function fetchResources(
   fetch: (request: RequestInfo, init?: RequestInit) => Promise<Response>,
   options: FetchResourcesOptions = {}
-): Promise<{ resources: Resource[]; filter?: ResolvedFilterOptions; timestamp: Date }> {
+): Promise<{
+  ok: boolean;
+  resources: Resource[];
+  filter?: ResolvedFilterOptions;
+  timestamp?: Date;
+}> {
   const { baseURL = DefaultBaseURL, retry = 1 } = options;
 
   const url = stringifySearchURL(baseURL, options);
@@ -33,12 +38,18 @@ export async function fetchResources(
     const count = options.count < 0 ? Number.MAX_SAFE_INTEGER : options.count;
 
     const map = new Map<string, Resource>();
+    let aborted = false;
     let timestamp = new Date(0);
     let filter: ResolvedFilterOptions | undefined = undefined;
 
     for (let page = 1; map.size < count; page++) {
       try {
         const resp = await fetchPage(page);
+        if (!resp) {
+          aborted = true;
+          break;
+        }
+
         timestamp = resp.timestamp;
         if (resp.filter) {
           filter = resp.filter;
@@ -57,6 +68,7 @@ export async function fetchResources(
         await options.progress?.(newRes, { url: url.toString(), page, timestamp });
       } catch (error) {
         if (error instanceof Error && error.name === 'AbortError') {
+          aborted = true;
           break;
         } else {
           throw error;
@@ -65,35 +77,52 @@ export async function fetchResources(
     }
 
     return {
+      ok: !aborted,
       resources: uniq([...map.values()]),
       filter,
       timestamp
     };
   } else {
-    const r = await fetchPage(options.page ?? 1);
-    const resources = uniq(r.resources);
-    await options.progress?.(resources, { url: url.toString(), page: 1, timestamp: r.timestamp });
+    const resp = await fetchPage(options.page ?? 1);
+    if (!resp) {
+      return {
+        ok: false,
+        resources: [],
+        filter: undefined,
+        timestamp: undefined
+      };
+    }
+
+    const resources = uniq(resp.resources);
+    await options.progress?.(resources, {
+      url: url.toString(),
+      page: 1,
+      timestamp: resp.timestamp
+    });
 
     return {
+      ok: true,
       resources,
-      filter: r.filter,
-      timestamp: r.timestamp
+      filter: resp.filter,
+      timestamp: resp.timestamp
     };
   }
 
   async function fetchPage(page: number) {
     url.searchParams.set('page', '' + page);
-    return await retryFn(
-      () =>
-        fetch(url.toString(), { signal: options.signal })
-          .then((r) => r.json())
-          .then((r) => ({
-            resources: r.resources as Resource[],
-            filter: r.filter as ResolvedFilterOptions | undefined,
-            timestamp: new Date(r.timestamp)
-          })),
-      retry
-    );
+    return await retryFn(async () => {
+      const resp = await fetch(url.toString(), { signal: options.signal });
+      if (resp.ok) {
+        const r = await resp.json();
+        return {
+          resources: r.resources as Resource[],
+          filter: r.filter as ResolvedFilterOptions | undefined,
+          timestamp: new Date(r.timestamp)
+        };
+      } else {
+        return undefined;
+      }
+    }, retry);
   }
 
   function uniq(resources: Resource[]) {
