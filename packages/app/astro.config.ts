3203ee8e3f5e3cbd6c2e5c1190d9672e9cd26279
XLor <yjl9903@vip.qq.com>
2023-11-14 16:24:40 +0800

    feat(app): refactor seo

--- packages/app/astro.config.ts
+++ packages/app/astro.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
       policy: [
         {
           userAgent: '*',
-          allow: '/resource/',
+          allow: ['/resource/', '/anime/', '/docs/'],
           disallow: ['/resources/', '/api/', '/rss/']
         }
       ]
