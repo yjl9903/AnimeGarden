1f67e20f2a69864dcde3de97680f6bba7f321e01
XLor <yjl9903@vip.qq.com>
2024-01-15 16:50:56 +0800

    feat(server): test database connection

---
+++ packages/server/src/database.ts
@@ -0,0 +1,11 @@
+import { connectDatabase } from '@animegarden/database';
+
+const DATABASE_URI = process.env.POSTGRES_URI ?? process.env.DATABASE_URI;
+if (!DATABASE_URI) {
+  console.log(`Can not find database connection string`);
+  process.exit(1);
+}
+
+const { database, connection } = connectDatabase(DATABASE_URI);
+
+export { database, connection };
