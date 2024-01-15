b190c6cb2369c1db1463a829272c665b0b896e99
XLor <yjl9903@vip.qq.com>
2024-01-16 00:40:12 +0800

    fix(database): sort by created at

--- packages/database/src/operations/sync.ts
+++ packages/database/src/operations/sync.ts
@@ -5,7 +5,11 @@ import type { Database } from '../connection';
 import { insertResourceDocuments } from '../meilisearch';
 
 export async function syncResourcesToMeili(database: Database, meili: MeiliSearch) {
-  const res = await database.query.resources.findMany({ offset: 0, limit: 1000 });
+  const res = await database.query.resources.findMany({
+    offset: 0,
+    limit: 1000,
+    orderBy: (t, { desc }) => [desc(t.createdAt)]
+  });
   await insertResourceDocuments(meili, res);
   return { count: res.length };
 }
