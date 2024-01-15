b9d4c8a8cf80fc620b205a26f5d61c1a569053d1
XLor <yjl9903@vip.qq.com>
2024-01-15 16:05:11 +0800

    feat: add docker file

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,11 +2,17 @@ import { Hono } from 'hono';
 import { logger } from 'hono/logger';
 import { serve } from '@hono/node-server';
 
-const app = new Hono();
+const app = new Hono({});
 
 app.all('*', logger());
 app.get('/', (c) => c.text('Hello Node.js!'));
 
-serve(app, (info) => {
-  console.log(`Listening http://${info.address}:${info.port}`);
-});
+serve(
+  {
+    fetch: app.fetch,
+    port: process.env.port ? +process.env.port : 3000
+  },
+  (info) => {
+    console.log(`Listening http://${info.address}:${info.port}`);
+  }
+);
