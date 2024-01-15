30690a2ddd4e1c81b0c7a10c2556ac56d52002d5
XLor <yjl9903@vip.qq.com>
2024-01-15 16:53:41 +0800

    fix(server): migrate command

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -3,13 +3,14 @@ import { app } from './app';
 import { serve } from '@hono/node-server';
 
 import { logger } from './logger';
+import { connection } from './database';
 
 serve(
   {
     fetch: app.fetch,
     port: process.env.port ? +process.env.port : 3000
   },
-  (info) => {
+  async (info) => {
     logger.info(null, `Listening http://${info.address}:${info.port}`);
   }
 );
