092f65294766b0f9605a0b54421752ff495d9701
XLor <yjl9903@vip.qq.com>
2024-01-15 15:29:43 +0800

    feat(server): init structure

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,1 +1,8 @@
-export const hello = 1;
+import { Hono } from 'hono';
+import { serve } from '@hono/node-server';
+
+const app = new Hono();
+
+app.get('/', (c) => c.text('Hello Node.js!'));
+
+serve(app);
--- packages/worker/src/database/types.ts
+++ packages/worker/src/database/types.ts
@@ -1,7 +1,8 @@
 import type { ColumnType } from 'kysely';
-export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
-  ? ColumnType<S, I | undefined, U>
-  : ColumnType<T, T | undefined, T>;
+export type Generated<T> =
+  T extends ColumnType<infer S, infer I, infer U>
+    ? ColumnType<S, I | undefined, U>
+    : ColumnType<T, T | undefined, T>;
 export type Timestamp = ColumnType<Date, Date | string, Date | string>;
 
 export type Resource = {
