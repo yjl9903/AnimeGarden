d536f38ebadd00c2374cd28f6752ff7979f5b4c0
XLor <yjl9903@vip.qq.com>
2024-01-29 21:45:46 +0800

    feat(app): add toast

--- packages/app/src/logic/notification.ts
+++ packages/app/src/logic/notification.ts
@@ -1,7 +1,24 @@
 import { toast } from 'sonner';
+import { committerDate as latestDate } from '~build/git';
 
 import { committerDate } from '../state';
 
+const event = {
+  date: new Date(`Mon Jan 29 2024 21:37:26 GMT+0800`),
+  toast: () => {
+    const content = `AnimeGarden 近期正在进行代码重构和服务器迁移.\nRSS 订阅链接需要重新获取.`;
+    toast(`AnimeGarden 更新通知`, {
+      description: content,
+      important: true,
+      closeButton: true,
+      duration: 10 * 1000
+    });
+  }
+};
+
 document.addEventListener('astro:page-load', () => {
-  console.log('commiter', committerDate.get());
+  const date = committerDate.get() ? committerDate.get() : new Date(0);
+  if (date.getTime() < event.date.getTime()) {
+    event.toast();
+  }
 });
