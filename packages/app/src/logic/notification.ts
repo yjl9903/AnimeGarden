9915922f0fe57b00689a45e447efae6d16ce3709
XLor <yjl9903@vip.qq.com>
2024-01-29 22:11:07 +0800

    chore: update notification

--- packages/app/src/logic/notification.ts
+++ packages/app/src/logic/notification.ts
@@ -6,7 +6,12 @@ import { committerDate } from '../state';
 const event = {
   date: new Date(`Mon Jan 29 2024 21:37:26 GMT+0800`),
   toast: () => {
-    const content = `AnimeGarden 近期正在进行代码重构和服务器迁移.\nRSS 订阅链接需要重新获取.`;
+    const content = [
+      `AnimeGarden 近期正在进行代码重构和服务器迁移.`,
+      `优化了搜索的响应速度.`,
+      `部分 RSS 订阅链接可能需要重新获取.`,
+      `问题反馈: https://github.com/yjl9903/AnimeGarden/issues`
+    ].join('\n');
     toast(`AnimeGarden 更新通知`, {
       description: content,
       important: true,
