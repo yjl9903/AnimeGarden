f0ce5f659db20487027855e8db5883c0a89ec638
XLor <yjl9903@vip.qq.com>
2024-02-04 21:28:40 +0800

    feat(app): filter out china anime

--- packages/app/src/logic/anime.ts
+++ packages/app/src/logic/anime.ts
@@ -10,19 +10,20 @@ export const Weekday = (new Date(new Date().getTime() - 6 * 60 * 60 * 1000).getD
 
 export const calendar = rawCalendar.map((_, idx) => {
   const index = (idx + Weekday) % 7;
+
+  const isChina = (bgm: FullBangumi) => {
+    const cn = ['国创', '国产动画', '国漫', '中国'];
+    return bgm.bangumi?.tags.some((t) => cn.includes(t)) ? 1 : 0;
+  };
+
   return {
     index: index + 1,
     text: ['一', '二', '三', '四', '五', '六', '日'][index],
     bangumis: rawCalendar[index]
       .filter((b) => !!getPosterImage(b))
+      .filter((b) => !isChina(b))
       .sort((lhs, rhs) => {
-        const getLanguage = (bgm: FullBangumi) => {
-          const cn = ['国创', '国产动画', '国漫', '中国'];
-          return bgm.bangumi?.tags.some((t) => cn.includes(t)) || bgm.bangumi?.name_cn === ''
-            ? 1
-            : 0;
-        };
-        const lang = getLanguage(lhs) - getLanguage(rhs);
+        const lang = isChina(lhs) - isChina(rhs);
         if (lang !== 0) return lang;
         return new Date(rhs.air_date).getTime() - new Date(lhs.air_date).getTime();
       })
