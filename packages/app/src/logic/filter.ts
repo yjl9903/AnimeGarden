6bd330073b0f9a421e7a4241347eb750c420d6b6
XLor <yjl9903@vip.qq.com>
2024-02-08 15:19:34 +0800

    feat(app): extract filter panel

---
+++ packages/app/src/logic/filter.ts
@@ -0,0 +1,39 @@
+import { findFansub, type ResolvedFilterOptions, type ResourceType } from 'animegarden';
+
+import { removeQuote } from '@/utils';
+import { DisplayType, DisplayTypeColor, QueryType } from '@/constant';
+
+export type DisplayResolvedFilterOptions = ReturnType<typeof resolveFilterOptions>;
+
+export function resolveFilterOptions(filter: Omit<ResolvedFilterOptions, 'page'>) {
+  const fansubId = filter.fansubId;
+  const fansubs = fansubId
+    ? fansubId.map((id) => {
+        const provider = 'dmhy';
+        const fs = findFansub(provider, id);
+        return fs ? fs : { provider, providerId: id, name: id };
+      })
+    : undefined;
+
+  const rawType = (
+    filter.type && filter.type in QueryType ? QueryType[filter.type] : filter.type
+  ) as ResourceType | undefined;
+  const type = rawType in DisplayType ? DisplayType[rawType] : rawType;
+
+  return {
+    publisher: filter.publisherId,
+    fansubs,
+    type: rawType
+      ? {
+          name: type,
+          color: DisplayTypeColor[type] ?? DisplayTypeColor[rawType]
+        }
+      : undefined,
+    before: filter.before,
+    after: filter.after,
+    search: filter.search ? removeQuote(filter.search) : [],
+    include: filter.include ?? [],
+    keywords: filter.keywords ?? [],
+    exclude: filter.exclude ?? []
+  };
+}
