9ca9b093c88706d0fad07e44148109b47dca857c
XLor <yjl9903@vip.qq.com>
2024-02-19 00:50:47 +0800

    feat(app): request log middleware

--- packages/app/src/middleware/index.ts
+++ packages/app/src/middleware/index.ts
@@ -1,23 +1,34 @@
 import { defineMiddleware } from 'astro:middleware';
 
+import { bold, dim } from '@breadc/color';
+
 export const onRequest = defineMiddleware(async (context, next) => {
   const url = new URL(context.request.url);
-  const timer = createTimer(`Render ${url.pathname}`);
+  const timer = createTimer();
   timer.start();
   const response = await next();
   timer.end();
+
+  console.log(
+    `${bold(context.request.method)} ${url.pathname} ${dim(timer.duration())}  <-- ${dim(`${context.request.referrer} ${context.clientAddress}`)}`
+  );
+
   return response;
 });
 
-function createTimer(label: string) {
+function createTimer() {
   let start = new Date();
+  let end: Date | undefined = undefined;
+
   return {
     start() {
       start = new Date();
     },
     end() {
-      const end = new Date();
-      console.log(`${label}: ${((end.getTime() - start.getTime()) / 1000).toFixed(0)}ms`);
+      end = new Date();
+    },
+    duration() {
+      return `${((end.getTime() - start.getTime()) / 1000).toFixed(0)}ms`;
     }
   };
 }
