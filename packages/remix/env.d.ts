6a920df19402a605ca942aab2de85bbe0c806ca1
XLor <yjl9903@vip.qq.com>
2024-07-24 00:56:10 +0800

    feat(remix): add api routes

---
+++ packages/remix/env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="unplugin-info/client" />
+
+declare module '~build/meta' {
+  export const APP_HOST: string;
+
+  export const SERVER_URL: string;
+}
--- packages/remix/vite.config.ts
+++ packages/remix/vite.config.ts
@@ -2,10 +2,26 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-import UnoCSS from 'unocss/vite'
+import UnoCSS from 'unocss/vite';
+import Info from 'unplugin-info/vite';
+
+const APP_HOST = process.env.APP_HOST ?? `garden.breadio.wiki`;
+const SERVER_URL = process.env.SERVER_URL ?? `https://garden.breadio.wiki/api/`;
 
 export default defineConfig({
   plugins: [
+    Info({
+      meta: {
+        /**
+         * The deployed host of animegarden
+         */
+        APP_HOST,
+        /**
+         * The backend server url of animegarden
+         */
+        SERVER_URL
+      }
+    }),
     UnoCSS(),
     remix({
       future: {
