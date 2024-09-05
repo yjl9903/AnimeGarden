8e2a95ab7f57250f2b82baeae643011e515ba841
XLor <yjl9903@vip.qq.com>
2024-09-05 22:49:56 +0800

    feat(app): migrate to keep share

--- packages/app/src/utils.ts
+++ packages/app/src/utils.ts
@@ -10,7 +10,8 @@ export function getRuntimeEnv(locals: App.Locals): Env | undefined {
 }
 
 export function getPikPakUrlChecker(magnet: string) {
-  const prefix = magnet.split('&')[0];
-  const replaced = prefix.replace(/^magnet:\?xt/, 'magnet:?xt.1');
-  return `https://mypikpak.com/drive/url-checker?url=${replaced}`;
+  const url = magnet.split('&')[0];
+  return 'https://keepshare.org/gv78k1oi/' + encodeURIComponent(url);
+  // const replaced = prefix.replace(/^magnet:\?xt/, 'magnet:?xt.1');
+  // return `https://mypikpak.com/drive/url-checker?url=${replaced}`;
 }
