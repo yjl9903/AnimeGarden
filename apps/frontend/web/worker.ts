9459ded4f9e18b3a2f3652778884ebfbaf9aecd8
XLor <yjl9903@vip.qq.com>
2024-09-11 23:55:04 +0800

    chore(web): handle error

--- apps/frontend/web/worker.ts
+++ apps/frontend/web/worker.ts
@@ -18,4 +18,14 @@ const app = new Hono<ContextEnv>();
 
 app.use('*', staticAssets(), remix({ build: build as any }));
 
+app.onError((err, c) => {
+  if (err.message) {
+    console.log(...err.message.trim().split('\n'));
+  }
+  if (err.stack) {
+    console.log(...err.stack.trim().split('\n'));
+  }
+  return c.json({ status: 500, messsage: err?.message ?? 'Internal Error' }, 500);
+});
+
 export default app;
