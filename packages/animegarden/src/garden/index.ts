10b0d5fcbb64ee9eac9981c89a4305a21aa06457
XLor <yjl9903@vip.qq.com>
2024-09-20 23:55:11 +0800

    Revert "chore: debug"
    This reverts commit 332b3cb55d10205a0ea726b97f6d9147543a8ee7.

--- packages/animegarden/src/garden/index.ts
+++ packages/animegarden/src/garden/index.ts
@@ -154,8 +154,6 @@ export async function fetchResources<T extends FetchResourcesOptions = FetchReso
         headers.set(`user-agent`, `animegarden@${version}`);
       }
 
-      console.log('Fetch resources:', url.toString());
-
       const resp = await fetch(url.toString(), {
         headers,
         signal: options.signal
