8b224953a8a12415965e476818702f5999725415
yjl9903 <yjl9903@onekuma.cn>
2024-10-04 19:57:14 +0800

    chore: init collection filter page

---
+++ apps/frontend/web/app/routes/collection.filter.$filter/route.tsx
@@ -0,0 +1,19 @@
+import type { MetaFunction } from '@remix-run/cloudflare';
+
+import Layout from '~/layouts/Layout';
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: 'Anime Garden 動漫花園資源網第三方镜像站' },
+    { name: 'description', content: '}Anime Garden 動漫花園資源網第三方镜像站' }
+  ];
+};
+
+export default function Resources() {
+  return (
+    <Layout>
+      <div className="w-full pt-12 pb-24">
+      </div>
+    </Layout>
+  );
+}
