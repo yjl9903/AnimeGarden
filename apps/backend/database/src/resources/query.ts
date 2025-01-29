f10bd89715838f45c99ce8c1f40d29d68247fbb3
yjl9903 <yjl9903@onekuma.cn>
2025-01-30 00:27:34 +0800

    fix(web): footer style

--- apps/backend/database/src/resources/query.ts
+++ apps/backend/database/src/resources/query.ts
@@ -612,7 +612,7 @@ export class Task {
   }
 
   public insertResources(resp: DatabaseResource[]) {
-    const visited = new Set(this.resources.map(r => r.id));
+    const visited = new Set(this.resources.map((r) => r.id));
     let changed = false;
     for (const r of resp) {
       if (!visited.has(r.id) && this.conds.every((c) => c(r))) {
--- apps/frontend/web/app/layouts/Footer.tsx
+++ apps/frontend/web/app/layouts/Footer.tsx
@@ -13,13 +13,14 @@ export interface FooterProps {
 export const Footer = memo((props: FooterProps) => {
   const isOpen = useAtomValue(isOpenSidebar);
 
-  const ref = useRef<string>();
+  const ref = useRef<{ timestamp?: string }>({});
   const { timestamp: timestampStr } = props;
   if (timestampStr) {
-    ref.current = timestampStr;
+    if (!ref.current) ref.current = {};
+    ref.current.timestamp = timestampStr;
   }
   const timestamp =
-    (timestampStr ?? ref.current) ? new Date(timestampStr ?? ref.current!) : undefined;
+    (timestampStr ?? ref.current) ? new Date(timestampStr ?? ref.current.timestamp!) : undefined;
 
   return (
     <footer
@@ -32,48 +33,52 @@ export const Footer = memo((props: FooterProps) => {
       )}
     >
       <div className={clsx('main', 'w-full')}>
-        <div className={clsx('[&_a:hover]:underline', 'text-main-600')}>
+        <div className={clsx('[&_a:hover]:underline', 'text-main-600', 'lt-sm:text-sm')}>
           <div className="flex">
             <span className="text-main-900 font-bold select-none">状态</span>
-            <span className="i-carbon:chevron-right text-xl text-main-900 font-bold select-none relative top-[2px]"></span>
-            <span className="ml-4 ">
+            <span className="i-carbon:chevron-right text-xl lt-sm:text-base text-main-900 font-bold select-none relative top-[2px]"></span>
+            <span className="ml-2 lt-sm:ml-2">
               {timestamp ? '数据更新于 ' + formatChinaTime(timestamp) : '服务器错误'}
             </span>
           </div>
           <div className="flex mt-2">
             <span className="text-main-900 font-bold select-none">关于</span>
-            <span className="i-carbon:chevron-right text-xl text-main-900 font-bold select-none relative top-[2px]"></span>
-            <a href="https://animespace.onekuma.cn" className="ml-4 ">
+            <span className="i-carbon:chevron-right text-xl lt-sm:text-base text-main-900 font-bold select-none relative top-[2px]"></span>
+            <a href="https://animespace.onekuma.cn" className="ml-2 lt-sm:ml-2">
               关于本站
             </a>
-            <a href="https://github.com/yjl9903/AnimeGarden" target="_blank" className="ml-4">
+            <a
+              href="https://github.com/yjl9903/AnimeGarden"
+              target="_blank"
+              className="ml-4 lt-sm:ml-2"
+            >
               GitHub
             </a>
-            <a href="https://animespace.onekuma.cn" target="_blank" className="ml-4">
+            <a href="https://animespace.onekuma.cn" target="_blank" className="ml-4 lt-sm:ml-2">
               AnimeSpace 计划
             </a>
           </div>
           <div className="flex mt-2">
             <span className="text-main-900 font-bold select-none">更多</span>
-            <span className="i-carbon:chevron-right text-main-900 text-xl font-bold select-none relative top-[2px]"></span>
-            <a href="https://t.me/animegarden_dev" target="_blank" className="ml-4">
+            <span className="i-carbon:chevron-right lt-sm:text-base text-main-900 text-xl font-bold select-none relative top-[2px]"></span>
+            <a href="https://t.me/animegarden_dev" target="_blank" className="ml-2 lt-sm:ml-2">
               Telegram
             </a>
             <a
               href="https://github.com/yjl9903/AnimeGarden/issues"
               target="_blank"
-              className="ml-4"
+              className="ml-4 lt-sm:ml-2"
             >
               问题反馈
             </a>
             <a
               href="https://animespace.onekuma.cn/animegarden/search"
               target="_blank"
-              className="ml-4"
+              className="ml-4 lt-sm:ml-2"
             >
               帮助文档
             </a>
-            <a href="https://status.onekuma.cn/" target="_blank" className="ml-4">
+            <a href="https://status.onekuma.cn/" target="_blank" className="ml-4 lt-sm:ml-2">
               监控
             </a>
           </div>
