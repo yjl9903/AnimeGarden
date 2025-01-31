c75db86b406980b7d0f7b4fd93081c8346f9517f
yjl9903 <yjl9903@onekuma.cn>
2025-01-31 20:19:40 +0800

    chore: cleanup scripts

--- scripts/search.ts
+++ scripts/search.ts
@@ -1,11 +1,28 @@
-import { ufetch } from '@animegarden/cli';
-import { fetchResources } from '../packages/animegarden/src';
+import { setGlobalDispatcher, ProxyAgent } from 'undici';
+
+import { fetchResources } from '../packages/client/src';
 
 async function main() {
-  const r = await fetchResources(ufetch, {
-    include: ['葬送的芙莉蓮']
+  const proxy = getProxy();
+  if (proxy) setGlobalDispatcher(new ProxyAgent(proxy));
+  const r = await fetchResources({
+    search: ['葬送的芙莉蓮'],
+    pageSize: 1
   });
   console.log(r.resources);
+}
+
+function getProxy() {
+  const env = process?.env ?? {};
+  const list = ['HTTPS_PROXY', 'https_proxy', 'HTTP_PROXY', 'http_proxy'];
+  for (const l of list) {
+    // @ts-ignore
+    const t = env[l];
+    if (!!t) {
+      return t;
+    }
+  }
+  return undefined;
 }
 
 main();
