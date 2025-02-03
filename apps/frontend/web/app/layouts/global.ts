c4f4438fb1713d29af64471195bc439e0cdd4cec
yjl9903 <yjl9903@onekuma.cn>
2025-02-03 23:06:31 +0800

    chore: remove log

--- apps/frontend/web/app/layouts/global.ts
+++ apps/frontend/web/app/layouts/global.ts
@@ -8,8 +8,6 @@ let heroSearch = document.querySelector('#hero-search');
 let heroPlaceholder = document.querySelector('#hero-placeholder');
 let sidebarRoot: HTMLDivElement | null = document.querySelector('.sidebar-root');
 function updateHero() {
-  console.log('=== update hero', document.documentElement.scrollTop);
-
   const y = document.documentElement.scrollTop;
 
   if (!header || !header.isConnected) {
