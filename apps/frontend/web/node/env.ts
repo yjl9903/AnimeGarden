3defec582343cd0a1bd06bfb3a2b1e56fc0dc41c
XLor <yjl9903@vip.qq.com>
2025-04-09 22:47:20 +0800

    chore: update dockerfile

--- apps/frontend/web/node/env.ts
+++ apps/frontend/web/node/env.ts
@@ -1,8 +1,8 @@
 export const KNOWN_ENV = {
   NODE_ENV: 'production',
   KEEPSHARE: 'gv78k1oi',
-  APP_HOST: 'test.animes.garden',
-  SERVER_URL: 'https://test-api.animes.garden/'
+  APP_HOST: 'animes.garden',
+  SERVER_URL: 'https://api.animes.garden/'
 };
 
 export function env(ctx: Record<string, any> = process.env): typeof KNOWN_ENV {
