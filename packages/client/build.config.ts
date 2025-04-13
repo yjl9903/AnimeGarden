15c4bfc060bc1d8acbf9429f5554617cc8875c7b
XLor <yjl9903@vip.qq.com>
2025-04-14 02:57:52 +0800

    chore: fix build config

--- packages/client/build.config.ts
+++ packages/client/build.config.ts
@@ -1,9 +1,14 @@
+import path from 'node:path';
+
 import { defineBuildConfig } from 'unbuild';
 
 export default defineBuildConfig({
   entries: ['src/index'],
   declaration: true,
   clean: true,
+  alias: {
+    '@animegarden/shared': path.resolve(import.meta.dirname, '../shared/src/index.ts')
+  },
   rollup: {
     emitCJS: true,
     inlineDependencies: ['@animegarden/shared']
--- packages/scraper/build.config.ts
+++ packages/scraper/build.config.ts
@@ -1,10 +1,14 @@
+import path from 'node:path';
+
 import { defineBuildConfig } from 'unbuild';
 
 export default defineBuildConfig({
   entries: ['src/index'],
   declaration: true,
-  sourcemap: true,
   clean: true,
+  alias: {
+    '@animegarden/shared': path.resolve(import.meta.dirname, '../shared/src/index.ts')
+  },
   rollup: {
     emitCJS: true,
     inlineDependencies: ['@animegarden/shared', 'simptrad']
