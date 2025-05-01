6557ef9fb386ee052980a33005ec1fb3743bbd3f
XLor <yjl9903@vip.qq.com>
2025-05-01 09:26:12 +0800

    chore: update

--- apps/backend/manager/manager.sh
+++ apps/backend/manager/manager.sh
@@ -2,17 +2,17 @@
 
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
-node --max-old-space-size=256 --heap-prof --heapsnapshot-near-heap-limit=10 "$SCRIPT_DIR/cli.mjs" "$@"
+node --max-old-space-size=256 --heap-prof "$SCRIPT_DIR/cli.mjs" "$@"
 
 EXIT_CODE=$?
 
 if [ $EXIT_CODE -ne 0 ]; then
-  HEAPDUMP_FILES=$(find . -type f -name "*.heapsnapshot")
+  HEAPDUMP_FILES=$(find . -type f -name "*.heapprofile")
   for HEAPDUMP_FILE in $HEAPDUMP_FILES; do
     node "$SCRIPT_DIR/upload.mjs" "$HEAPDUMP_FILE" || true
   done
 
-  HEAPDUMP_FILES=$(find . -type f -name "*.heapprofile")
+  HEAPDUMP_FILES=$(find . -type f -name "*.heapsnapshot")
   for HEAPDUMP_FILE in $HEAPDUMP_FILES; do
     node "$SCRIPT_DIR/upload.mjs" "$HEAPDUMP_FILE" || true
   done
