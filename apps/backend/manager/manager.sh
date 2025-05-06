476070ad69619139ad0963417d1659cc135e57c3
yjl9903 <yjl9903@onekuma.cn>
2025-05-06 19:05:01 +0800

    feat(manager): start script

--- apps/backend/manager/manager.sh
+++ apps/backend/manager/manager.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
@@ -7,15 +7,21 @@ node --max-old-space-size=256 --heap-prof "$SCRIPT_DIR/cli.mjs" "$@"
 EXIT_CODE=$?
 
 if [ $EXIT_CODE -ne 0 ]; then
+  echo 'Start uploading heap dump file'
+
   HEAPDUMP_FILES=$(find . -type f -name "*.heapprofile")
   for HEAPDUMP_FILE in $HEAPDUMP_FILES; do
+    echo 'Uploading $HEAPDUMP_FILE'
     node "$SCRIPT_DIR/upload.mjs" "$HEAPDUMP_FILE" || true
   done
 
   HEAPDUMP_FILES=$(find . -type f -name "*.heapsnapshot")
   for HEAPDUMP_FILE in $HEAPDUMP_FILES; do
+    echo 'Uploading $HEAPDUMP_FILE'
     node "$SCRIPT_DIR/upload.mjs" "$HEAPDUMP_FILE" || true
   done
+
+  echo 'Finish uploading heap dump file'
 
   exit $EXIT_CODE
 fi
