bf7fb1da9e9a6ae89a48287357e854ce7ce2b668
yjl9903 <yjl9903@onekuma.cn>
2025-05-07 13:19:18 +0800

    chore: disable dump heap

--- apps/backend/manager/manager.sh
+++ apps/backend/manager/manager.sh
@@ -2,7 +2,8 @@
 
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
-node --max-old-space-size=128 --heap-prof --heapsnapshot-near-heap-limit=10 "$SCRIPT_DIR/cli.mjs" "$@"
+# node --max-old-space-size=128 --heap-prof --heapsnapshot-near-heap-limit=10 "$SCRIPT_DIR/cli.mjs" "$@"
+node "$SCRIPT_DIR/cli.mjs" "$@"
 
 EXIT_CODE=$?
 
