9efaa8db4a40ceb4e3c3ce84de000b30f0ed78ae
XLor <yjl9903@vip.qq.com>
2025-07-11 02:11:09 +0800

    feat(web): auto reload when resources error

--- apps/frontend/web/app/layouts/global.ts
+++ apps/frontend/web/app/layouts/global.ts
@@ -114,3 +114,19 @@ window.scrollTo = (...args) => {
   updateHero();
   return r;
 };
+
+// 监听 script error
+window.addEventListener(
+  'error',
+  (e) => {
+    // @ts-ignore
+    if (e?.target?.tagName === 'SCRIPT') {
+      // 避免无限刷新
+      if (!sessionStorage.getItem('script_error_reloaded')) {
+        sessionStorage.setItem('script_error_reloaded', 'true');
+        location.reload();
+      }
+    }
+  },
+  true
+);
