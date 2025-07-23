8938dd5f1c92c9d9a95d6a6029a36779f7b2b40b
yjl9903 <yjl9903@onekuma.cn>
2025-07-23 20:49:16 +0800

    fix(web): subject error page

--- apps/web/app/routes/resources.($page)/Error.tsx
+++ apps/web/app/routes/resources.($page)/Error.tsx
@@ -1,8 +1,23 @@
-export function Error() {
+export interface ErrorProps {
+  message?: string | React.ReactNode;
+
+  children?: React.ReactNode;
+}
+
+export function Error({ message, children }: ErrorProps) {
   return (
     <div className="h-20 text-2xl text-red-700/80 flex items-center justify-center">
-      <span className="mr2 i-carbon-error" />
-      <span>发生错误</span>
+      <div>
+        <span className="mr2 i-carbon-error" />
+        <span>发生错误</span>
+        {message && (
+          <>
+            <span>:&nbsp;</span>
+            <span>{message}</span>
+          </>
+        )}
+      </div>
+      {children}
     </div>
   );
 }
