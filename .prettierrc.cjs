501a2a0a69d3eb7dcecfedfddaec200a8fb6fb33
yjl9903 <yjl9903@onekuma.cn>
2025-07-29 13:44:14 +0800

    chore: bump deps

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,14 +2,14 @@ module.exports = {
   semi: true,
   singleQuote: true,
   printWidth: 100,
-  trailingComma: "none",
-  plugins: [require.resolve("prettier-plugin-astro")],
-  overrides: [
-    {
-      files: "*.astro",
-      options: {
-        parser: "astro",
-      },
-    },
-  ],
+  trailingComma: 'none'
+  // plugins: [require.resolve("prettier-plugin-astro")],
+  // overrides: [
+  //   {
+  //     files: "*.astro",
+  //     options: {
+  //       parser: "astro",
+  //     },
+  //   },
+  // ],
 };
