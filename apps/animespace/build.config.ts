2dc12e6dc85ea23be227d61ee52d01d04b3b0468
yjl9903 <yjl9903@onekuma.cn>
2026-02-12 01:30:44 +0800

    chore: not build cjs

--- apps/animespace/build.config.ts
+++ apps/animespace/build.config.ts
@@ -3,8 +3,5 @@ import { defineBuildConfig } from 'unbuild';
 export default defineBuildConfig({
   entries: ['src/index', 'src/cli'],
   declaration: true,
-  clean: true,
-  rollup: {
-    emitCJS: true
-  }
+  clean: true
 });
