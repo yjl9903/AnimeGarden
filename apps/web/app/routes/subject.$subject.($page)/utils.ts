900f829b27a8562d2263b9cd5733e5ad97357661
yjl9903 <yjl9903@onekuma.cn>
2026-03-08 23:46:45 +0800

    chore: update subject group

--- apps/web/app/routes/subject.$subject.($page)/utils.ts
+++ apps/web/app/routes/subject.$subject.($page)/utils.ts
@@ -2,26 +2,37 @@ import type { Resource } from '@animegarden/client';
 
 const FansubNames = [
   '雪飄工作室(FLsnow)',
+  '驯兽师联盟',
   '北宇治字幕组',
   'LoliHouse',
   '喵萌奶茶屋',
   'Prejudice-Studio',
   '三明治摆烂组',
+  'MoYuanCN',
+  '拨雪寻春',
   'SweetSub',
   'S1百综字幕组',
   '星空字幕组',
   '桜都字幕组',
   '悠哈C9字幕社',
   '悠哈璃羽字幕社',
-  '拨雪寻春',
   'MingYSub',
   '幻樱字幕组',
   '离谱Sub',
   '爱恋字幕社',
   '黑白字幕组',
   '绿茶字幕组',
+  '霜庭云花Sub',
+  '云光字幕组',
+  '云歌字幕组',
+  '六四位元字幕組',
+  '阿特拉斯字幕组',
+  '晚街与灯',
+  '极影字幕社',
+  '猎户发布组',
   'ANi',
-  '六四位元字幕組'
+  'DBD制作组',
+  'VCB-Studio'
 ];
 
 const FansubOrder = new Map<string, number>(FansubNames.map((name, i) => [name, i]));
